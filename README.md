6c725f542d9325781c759749e8d604fd3a1fa227
Jannatul Ferdous <jannatul.ferdous@stonybrook.edu>
2022-12-12 06:38:36 -0500

    final updates

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# dashboard_streamlit_new
-HHA Assignment 10
+# dashboard_streamlit
+HHA 507 Assignment 10
+
+Analysis of FIFA Worldcup 2022 Players Stats and Playing time
+
+pip3 install streamlit
+
+Create a VM in AZURE, copy the IP address and open in terminal (AZURE preffered):
+1. sudo apt-get update
+2. sudo apt install python3-pip
+3. git clone ###
+4. touch player_playingtime.py
+5. nano player_playingtime.py
+6. streamlit run player_playingtime.py
+7. obtained two datasets, added a title, headers, captions and performed a barchart to get the number of players receiving yellow cards and linechart analysis to get the number of players drawing goals
+
+# Important website
+https://docs.streamlit.io/library/get-started/create-an-app 
--- streamlit.py
+++ streamlit.py
@@ -1,1 +1,46 @@
+import pandas as pd
+import streamlit as st
+import numpy as np
+
+st.title('FIFA WORLDCUP 2022 Playing Time & Stats Data')
+
+st.caption('These datatsets are public and were retreived on Dec 12, 2022')
+#### Dataframe 1 - Head 25 ####
+#header
+st.header('FIFA WORLDCUP 2022 Player Playing Time')
+#### Caption ####
+st.caption('This dataset provides a good understanding of playing time for players in FIFA World CUP 2022')
+df1 = pd.read_csv('player_playingtime.csv')
+df1 = df1.head(25)
+df1
+
+
+## dataframe 2 - Head 25
+#header
+st.header('FIFA WOLDCUP 2022 Player Playing Time')
+#### Caption ####
+st.caption('This dataset provides a good understanding of stats for players in FIFA World CUP 2022')
+df2 = pd.read_csv('player_stats.csv')
+df2 = df2.head(100)
+df2
+
+#### Toggle ####
+
+if st.checkbox('Show first 100 records of FIFA 2022 Worldcup'):
+    st.dataframe(df1)
+
+# Displaying the dataframe
+if st.checkbox('player_stats'):
+    st.dataframe(df2)
+# Creating a barchart
+st.subheader('Display of  Number of Yellow Cards Received by Players')
+cards_yellow= df2['cards_yellow'].value_counts()
+st.bar_chart(cards_yellow)
+st.caption('Visual provides insight of players numbers getting number of yellow cards')
+
+# Creating a line chart
+st.subheader('Number of Goals')
+num_goals = df2['goals'].value_counts()
+st.bar_chart(num_goals)
+st.caption('Visual provides insight on the number of players drawing goals')
 
